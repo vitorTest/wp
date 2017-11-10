d2fc9a24ba0c425bcfeb63bc63d5f34d173d400f
Vitor Felix <vytorfelix2@gmail.com>
2017-11-10 16:09:03 -0300

    End of part 1

--- content.php
+++ content.php
@@ -1,10 +1,9 @@
 
 <div class="blog-post">
-            <h2 class="blog-post-title">Sample blog post</h2>
-            <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+            <h2 class="blog-post-title"><?php the_title(); ?></h2>
+            <p class="blog-post-meta"><?php echo the_date()." "; ?><a href="#"><?php the_author(); ?></a></p>
 
-            <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
-            <hr>
+            <?php the_content();?>
         </div><!-- /.blog-post -->
 
           
--- header.php
+++ header.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Blog Template for Bootstrap</title>
+    <title><?php echo get_bloginfo('name'); ?></title>
 
     <!-- Bootstrap core CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
@@ -30,11 +30,8 @@
     <div class="blog-masthead">
       <div class="container">
         <nav class="blog-nav">
-          <a class="blog-nav-item active" href="#">Home</a>
-          <a class="blog-nav-item" href="#">New features</a>
-          <a class="blog-nav-item" href="#">Press</a>
-          <a class="blog-nav-item" href="#">New hires</a>
-          <a class="blog-nav-item" href="#">About</a>
+          <!--<a class="blog-nav-item active" href="#">Home</a>-->
+          <?php wp_list_pages( '&title_li='); ?>
         </nav>
       </div>
     </div>
@@ -42,6 +39,6 @@
     <div class="container">
 
       <div class="blog-header">
-        <h1 class="blog-title">The Bootstrap Blog</h1>
-        <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
+        <h1 class="blog-title"><a href="<?php get_bloginfo('wp_url')?>"><?php echo get_bloginfo('name'); ?></a></h1>
+        <p class="lead blog-description"><?php echo get_bloginfo('description'); ?></p>
       </div>
