6dea1dae5fa253c62da894aa7825e1dd1e85e500
Vitor <vytorfelix2@gmail.com>
2019-06-26 00:27:46 -0300

    I Want to be the boy to warm your mothers heart

--- tanRascia/content-single.php
+++
@@ -1,9 +0,0 @@
-
-<div class="blog-post">
-            <h2 class="blog-post-title"><?php the_title(); ?></h2>
-            <p class="blog-post-meta"><?php echo the_date()." "; ?><a href="#"><?php the_author(); ?></a></p>
-
-            <?php the_content();?>
-        </div><!-- /.blog-post -->
-
-          
--- tanRascia/content.php
+++
@@ -1,9 +0,0 @@
-
-<div class="blog-post">
-            <h2 class="blog-post-title"><a href="<?php the_permalink()?>"><?php the_title(); ?></a></h2>
-            <p class="blog-post-meta"><?php echo the_date()." "; ?><a href="#"><?php the_author(); ?></a></p>
-
-            <?php the_excerpt();?>
-        </div><!-- /.blog-post -->
-
-          
